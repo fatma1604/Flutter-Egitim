2260d43b5ac6a2416a6a9075f5f42e9f9b267dbd
Fatma Nur Kamis <fatmanurkamis34@gmail.com>
2023-10-25 19:50:16 +0300

    odev.dart
    Dart ilk ödev

---
+++ odev.dart
@@ -0,0 +1,18 @@
+void main() {
+  int N = 5;
+  int sonuc = faktoriyelHesapla(N);
+  print("$N faktoriyeli: $sonuc");
+}
+
+int faktoriyelHesapla(int N) {
+  if (N == 0) {
+    return 1;
+  }
+  //fk
+
+  int faktoriyel = 1;
+  for (int i = 1; i <= N; i++) {
+    faktoriyel *= i;
+  }
+  return faktoriyel;
+}
