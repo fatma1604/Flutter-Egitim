5a516eeefc8a09654c04d98eb416a957a01bd4ea
Fatma Nur Kamis <fatmanurkamis34@gmail.com>
2023-11-10 11:39:51 +0300

    düzenleme
    düzenleme

---
+++ 1bTabeto/test_deneme/android/app/src/main/kotlin/com/example/test_deneme/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.test_deneme
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- widget_1calisma/android/app/src/main/kotlin/com/example/widget_1calisma/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.widget_1calisma
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
