b0a18013050d22bd8ceee731e7c4431d6ce291dc
Fatma Nur Kamis <fatmanurkamis34@gmail.com>
2024-01-22 20:14:21 +0300

    chat
    mesajlasma uygulaması yaptık,
    kulanıcı kayıtlı değilse hata alıyor ,kayıt olan kulanıcı ikinci kayıt olan kullanıcıyla mesajlasa biliyor

--- tobetowork/lib/componts/chat_page.dart
+++ tobetowork/lib/componts/chat_page.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:tobetowork/componts/chat_bubble.dart';
 import 'package:tobetowork/componts/my_texfild.dart';
 import 'package:tobetowork/service/chat_services.dart';
 import 'package:tobetowork/service/auth/auth_service.dart';
@@ -68,16 +69,17 @@ class ChatPage extends StatelessWidget {
     Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
     bool isCurrentUser = data['senderID'] == _autService.getCurrentUser()!.uid;
 
-    var alignment = isCurrentUser ? Alignment.center : Alignment.centerLeft;
+    var alignment =
+        isCurrentUser ? CrossAxisAlignment.end : CrossAxisAlignment.start;
     return Container(
-        alignment: alignment,
-        child: Column(
-          crossAxisAlignment:
-              isCurrentUser ? CrossAxisAlignment.end : CrossAxisAlignment.start,
-          children: [
-            Text(data["message"]),
-          ],
-        ));
+      padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+      child: Column(
+        crossAxisAlignment: alignment,
+        children: [
+          ChatBubble(message: data["message"], isCurrentUser: isCurrentUser)
+        ],
+      ),
+    );
   }
 
   Widget _buildUserInput() {
